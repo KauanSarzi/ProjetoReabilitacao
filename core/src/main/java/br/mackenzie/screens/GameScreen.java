a9d39ea8a5380fe4089c358557601cd1eb9b1f4f
Guilherme Shinohara <guiba.shinohara@gmail.com>
2025-10-20 22:03:24 -0300

    Adicionando a imagem de plano de fundo junto a classe GameScreen.java e Main.java arrumado de acordo com o GameScreen.java

--- core/src/main/java/br/mackenzie/screens/GameScreen.java
+++ core/src/main/java/br/mackenzie/screens/GameScreen.java
@@ -1,4 +1,52 @@
 package br.mackenzie.screens;
 
-public class GameScreen {
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class GameScreen implements Screen {
+    private SpriteBatch batch;
+    private Texture image;
+    private Texture background;
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+        image = new Texture("libgdx.png");
+        background = new Texture("background.png");
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        batch.begin();
+        // desenha o fundo primeiro
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        // depois a imagem
+        batch.draw(image, 140, 210);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {}
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {}
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        image.dispose();
+        background.dispose();
+    }
 }
+
